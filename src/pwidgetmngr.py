f3fb1bc20f0d0e5fd88c3f69b374ba7a85c2e4d7
marcelobarrosalmeida <marcelobarrosalmeida@e2c24ffe-286d-11de-bcab-dfcfbda92187>
2009-04-18 11:45:04 +0000

    Adding support for showing widgets in full screen

--- src/pwidgetmngr.py
+++ src/pwidgetmngr.py
@@ -15,27 +15,28 @@ class PWidgetMngr(object):
     def __init__(self):
         app.screen = "full"
         self.layouts = { u"4x3":self.layout_4x3 }
-        self.canvas_ctrl = None
         self.size = sysinfo.display_pixels()
-        self.canvas = None
         self.window_list = []
-        self.active_win = None
         self.bind_list = {}
         self.background = None
         self.active_focus = 0
+        self.active_widget = None
         self.widgets = {}         
-        self.canvas_ctrl = Canvas(redraw_callback = self.redraw,
-                                  event_callback = self.event,
-                                  resize_callback = self.resize)
+        self.canvas = Canvas(redraw_callback = self.redraw,
+                             event_callback = self.event,
+                             resize_callback = self.resize)
         self.size = sysinfo.display_pixels()
-        self.canvas = graphics.Image.new(self.size)
-        app.body = self.canvas_ctrl
-        self.menu = [ (u"Exit",self.close_app) ]
+        self.screen = graphics.Image.new(self.size)
+        self.screen_aux = graphics.Image.new(self.size)
+        app.body = self.canvas
+        self.menu = [(u"Exit",self.close_app) ]
         app.menu = self.menu
         self.lock = e32.Ao_lock()
         self.bind(self,key_codes.EKeyLeftArrow, self.focus_next)
         self.bind(self,key_codes.EKeyRightArrow, self.focus_prev)
+        self.bind(self,key_codes.EKeySelect, self.show_widget)
         app.exit_handler = self.close_app
+        
         self.load_widgets()
 
     def get_size(self):
@@ -63,14 +64,14 @@ class PWidgetMngr(object):
             if not self.bind_list.has_key(key):
                 self.bind_list[key] = {}
             self.bind_list[key][win] = {'win':win,'cbk':funct}
-            self.canvas_ctrl.bind(key,lambda: self.bind_dispatch(key))
+            self.canvas.bind(key,lambda: self.bind_dispatch(key))
 
     def bind_dispatch(self,key):
         for updt in self.bind_list[key].values():
             updt['cbk']()
        #     if updt['widget']:
        #         w=updt['win']
-       #         self.canvas.blit(w.get_canvas(),target=w.get_position(),source=((0,0),w.get_size()))
+       #         self.screen.blit(w.get_canvas(),target=w.get_position(),source=((0,0),w.get_size()))
        # if updt['widget']:                
        #     self.update_canvas()
             
@@ -101,22 +102,19 @@ class PWidgetMngr(object):
     def add_window(self,win):
         self.window_list.append(win)
         self.active_focus = len(self.window_list) - 1
-
-    def update_canvas(self):
-        #self.draw_focus()
-        self.canvas_ctrl.blit(self.canvas)
-        
+       
     def redraw(self,rect=None):
-        if self.background:
-            self.canvas.blit(self.background)
+        if self.active_widget:
+            self.canvas.blit(self.active_widget.get_canvas())
         else:
-            self.canvas.clear((255,255,255))
-        # generating a repaint respecting focus order
-        #nw = len(self.window_list)
-        #order = [ c%nw for c in range(self.active_focus+1,self.active_focus+1+nw) ]
-        if self.window_list:
-            self.layouts[u"4x3"](range(len(self.window_list)))
-        self.update_canvas()
+            if self.background:
+                self.screen.blit(self.background)
+            else:
+                self.screen.clear((255,255,255))            
+            if self.window_list:
+                self.layouts[u"4x3"](range(len(self.window_list)))
+            self.canvas.blit(self.screen)
+            #order = [ c%nw for c in range(self.active_focus+1,self.active_focus+1+nw) ]
         
     def layout_4x3(self,order):
         """
@@ -131,28 +129,22 @@ class PWidgetMngr(object):
             for lin in range(3):
                 # focus
                 if n == self.active_focus:
-                    self.canvas.rectangle((x-2,y-2,x+ww+2,y+wh+2),
+                    self.screen.rectangle((x-2,y-2,x+ww+2,y+wh+2),
                                           fill=(255,0,0),
                                           outline=(255,0,0))
                 if  n >=  len(self.window_list):
                     break
                 w = self.window_list[order[n]]
-                self.canvas.blit(w.get_canvas(),target=(x,y),source=((0,0),(ww,wh)))
+                # TODO: resize is generating exception ... async mode necessary
+                try:
+                    self.screen_aux = w.get_canvas().resize((ww,wh))
+                    self.screen.blit(self.screen_aux,target=(x,y),source=((0,0),(ww,wh)))
+                except:
+                    print "error: canvas resize"
                 x += ww + ws
                 n += 1
             y += wh + ws
-
-    #def draw_focus(self):
-    #    pass
-        """
-        if self.window_list:
-            w = self.window_list[self.active_focus]
-            p = list(w.get_position()[0:2])
-            p += [p[0]+5,p[1]+5]
-            self.canvas.rectangle(p,fill=(255,0,0),outline=(255,0,0))
-            w.got_focus()
-        """
-        
+       
     def event(self,ev):
         pass
 
@@ -170,9 +162,28 @@ class PWidgetMngr(object):
     def set_menu(self,menu):
         """ Merge window menu with PWidgetMngr menu
         """
-        m = menu + self.menu
+        m = menu + [(u"PyWidgets",self.show_main_screen)] + self.menu
         app.menu = m
 
+    def show_widget(self):
+        if self.window_list:
+            self.active_widget = self.window_list[self.active_focus]
+            self.active_widget.got_focus()
+            self.redraw()
+            
+    def show_main_screen(self):
+        app.menu = self.menu
+        self.active_widget = None
+        self.redraw()
+
+    def widget_is_full_screen(self):
+        """ Returns when an widget is in full screen mode or not
+        """
+        if self.active_widget:
+            return True
+        else:
+            return False
+        
     def set_title(self,title):
         app.title = title
         
