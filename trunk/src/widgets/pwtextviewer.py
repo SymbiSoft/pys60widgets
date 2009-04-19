2bc13ae87bdeb862d855d4bd5898bf61c3558cb3
javsmo <javsmo@e2c24ffe-286d-11de-bcab-dfcfbda92187>
2009-04-19 15:35:45 +0000

    Fixed some text wrap problems in PWTextViewer and added scrollbar draw.

--- trunk/src/widgets/pwtextviewer.py
+++ trunk/src/widgets/pwtextviewer.py
@@ -1,4 +1,5 @@
 import e32
+import re
 from pwidget import *
 from pwcolor import *
 from graphics import *
@@ -6,14 +7,12 @@ from appuifw import available_fonts, popup_menu
 
 __all__ = ["PWTextViewer"]
 
-LIPSUM = u"""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin feugiat, mi id consectetur cursus, sem diam pretium nibh, et tristique libero erat luctus odio. Morbi dapibus mauris sit amet lectus. Ut est eros, aliquam ut, dapibus sed, vehicula at, nisl. Ut turpis dui, consequat eget, congue et, consequat eu, massa. In hac habitasse platea dictumst. Pellentesque mi felis, hendrerit id, imperdiet ut, venenatis nec, velit. Suspendisse et tellus ut mauris bibendum tempus. Nullam molestie. Etiam lobortis. Maecenas sit amet nunc. Vivamus diam massa, tincidunt id, iaculis nec, dignissim quis, massa. Pellentesque tempor leo rutrum neque. Donec sit amet nisi a dui tristique eleifend. Nunc in mauris sed enim pulvinar iaculis. Vestibulum dui turpis, accumsan et, fermentum vitae, aliquam vitae, eros.
-
+LIPSUM = u"""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin feugiat, mi id consectetur cursus, sem diam pretium nibh, et tristique libero erat luctus odio. Morbi dapibus mauris sit amet lectus.
+Ut est eros, aliquam ut, dapibus sed, vehicula at, nisl. Ut turpis dui, consequat eget, congue et, consequat eu, massa. In hac habitasse platea dictumst. Pellentesque mi felis, hendrerit id, imperdiet ut, venenatis nec, velit. 
+Suspendisse et tellus ut mauris bibendum tempus. Nullam molestie. Etiam lobortis. Maecenas sit amet nunc. Vivamus diam massa, tincidunt id, iaculis nec, dignissim quis, massa. Pellentesque tempor leo rutrum neque. Donec sit amet nisi a dui tristique eleifend. Nunc in mauris sed enim pulvinar iaculis. Vestibulum dui turpis, accumsan et, fermentum vitae, aliquam vitae, eros.
 Sed mattis nisi at orci. Etiam ante orci, ornare sit amet, placerat feugiat, venenatis eu, dui. Sed pretium blandit ante. Curabitur feugiat orci condimentum enim. Quisque bibendum nulla eu massa. Maecenas tempor lobortis libero. Etiam lorem sapien, imperdiet at, vestibulum nec, molestie nec, dui. Etiam feugiat sem id augue ullamcorper luctus. Pellentesque gravida dictum dui. Pellentesque sit amet orci. Maecenas elit. Suspendisse cursus accumsan dui. Vestibulum mi magna, volutpat ut, ornare quis, vulputate sit amet, mi. Aenean a lectus sit amet ante malesuada egestas.
-
 Donec interdum. Nam urna nibh, auctor non, dictum eget, convallis eget, erat. Duis lorem arcu, varius nec, auctor ut, lobortis sit amet, leo. Quisque sodales egestas metus. Pellentesque mattis risus vel velit. Praesent porta turpis in ipsum. In sed quam et libero suscipit vestibulum. Donec ut sem. Nullam at mauris vel eros porttitor condimentum. Vestibulum vitae nibh et neque placerat bibendum. Maecenas ultricies orci ut ipsum. In hendrerit massa at lorem. Nunc fermentum dapibus felis. Nunc mauris. Ut vitae sem quis ligula malesuada congue. Donec luctus mauris sit amet urna. Morbi odio dolor, tincidunt at, ultrices ac, tincidunt at, urna. Integer et justo. Nulla enim elit, ornare nec, lacinia id, auctor sed, tellus.
-
 Mauris enim diam, lobortis at, eleifend malesuada, sodales sit amet, quam. Pellentesque porta leo nec arcu. Aliquam eget sapien quis urna fermentum ornare. Fusce posuere, est eu vehicula bibendum, odio tellus lobortis nunc, quis euismod justo ante a magna. Nulla nec odio. Morbi ac urna ut erat placerat mollis. Vestibulum sed sem et nisl dapibus fringilla. Nam mattis ante. In rutrum blandit eros. Pellentesque ut sem. Aliquam facilisis. Praesent leo erat, posuere eu, malesuada in, tincidunt sed, leo. Curabitur vitae nulla. Sed scelerisque vehicula est.
-
 Quisque nec diam. Vestibulum hendrerit. Nunc lacus erat, convallis sed, malesuada vel, interdum porttitor, diam. Curabitur viverra, tortor elementum placerat porta, turpis dolor mattis magna, vel tempor arcu metus in tortor. Maecenas ut dolor. Aliquam erat volutpat. Nullam a risus sed diam pellentesque feugiat. Pellentesque non turpis id justo malesuada consectetur. Integer nulla. Donec semper odio euismod nulla tincidunt varius. Suspendisse non lacus sit amet orci rutrum vulputate. Fusce sit amet urna quis nibh elementum sagittis. Proin blandit venenatis urna. Donec iaculis odio eget est. Sed eleifend semper massa. Nulla fringilla ligula at eros. Mauris lobortis sollicitudin odio. Etiam nunc. 
 """
 
@@ -24,6 +23,7 @@ class PWTextViewer(PWidget):
         menu = [(u"Font", self.change_font), (u"Colors", self.change_color)]
         PWidget.__init__(self,mngr,self.name, menu)
         self.check_default_values(attrs)
+        self.cursor = [0,0]
 
     def check_default_values(self, attrs):
         """ Given some user attributes, define all attributes
@@ -34,12 +34,16 @@ class PWTextViewer(PWidget):
                            'left':0,
                            'width':240,
                            'height':320,
-                           'bg_color':PWColor(YELLOW),
-                           'fg_color':PWColor(BLUE),
-                           'scrollbar_color':PWColor(BLACK),
-                           'scrollbar_width':5,
+                           'bg_color':PWColor([255,255,192,255]),
+                           'fg_color':PWColor([0,0,128,255]),
+                           'scrollbar_color':PWColor([0,0,128,255]),
+                           'scrollbar_width':6,
                            'font':u"dense",
                            'font_size':14,
+                           'margin_top':3,
+                           'margin_left':3,
+                           'margin_bottom':0,
+                           'margin_right':3,
                            'text': LIPSUM}
 
         for k in self.def_attrs.keys():
@@ -55,54 +59,75 @@ class PWTextViewer(PWidget):
 
     def run(self):
         self.add_window(self)
+        
+    def draw_cursor(self):
+        pass
+    
+    def draw_scrollbar(self):
+        handle_width = self.attrs['scrollbar_width']
+        glow_width = int(handle_width / 2)
+        shadow_width = handle_width - glow_width
+        shadow_color = PWColor(self.attrs['scrollbar_color'].get_color())
+        glow_color = PWColor(shadow_color.get_color())
+        white = PWColor(WHITE)
+        glow_color.combine(white, 0.3)
+        a = self.attrs
+        #Draw scrollbar BG
+        self.canvas.rectangle((a['width']-handle_width,a['top'],a['width'],a['height']),
+                                outline=shadow_color.get_color(),
+                                fill=None)
+        #Draw scrollbar handle - first the shadow
+        self.canvas.rectangle((a['width']-shadow_width,a['top'],a['width'],a['top']+30),
+                                outline=shadow_color.get_color(),
+                                fill=shadow_color.get_color())
+        #and now the glow
+        self.canvas.rectangle((a['width']-handle_width,a['top'],a['width']-handle_width+glow_width,a['top']+30),
+                                outline=glow_color.get_color(),
+                                fill=glow_color.get_color())
     
     def draw_background(self):
         self.canvas.clear(self.attrs['bg_color'].get_color())
     
-    def draw_scrollbar(self):
-        pass
-    
     def draw_text(self):
         i = 1;
         for line in self.lines:
-            self.canvas.text((0, i * self.attrs['font_size']),
+            self.canvas.text((self.attrs['margin_left'], self.attrs['margin_top'] + i * self.attrs['font_size']),
                                 line, 
                                 fill = self.attrs['fg_color'].get_color(), 
                                 font = (self.attrs['font'],self.attrs['font_size'],FONT_ANTIALIAS))
             i += 1
-    
+
     # modified version of TextRenderer.chop 
     # http://discussion.forum.nokia.com/forum/showthread.php?t=124666
     def text_wrap(self):
         self.lines = []
-        text_left = self.text
-        width = self.attrs['width'] - self.attrs['scrollbar_width'] 
-        while len(text_left) > 0: 
-            bounding, to_right, fits = self.canvas.measure_text(text_left,
-                                                         font=(self.attrs['font'],self.attrs['font_size'],FONT_ANTIALIAS),
-                                                         maxwidth=width,
-                                                         maxadvance=width)
-
-            if fits <= 0:
-                self.lines.append(text_left)
-                break
-
-            slice = text_left[0:fits]
-            adjust = 0 # (preserve or not whitespaces at the end of the row)
-
-            if len(slice) < len(text_left):
-                # find the separator character closest to the right
-                rindex = -1
-                idx = slice.rfind(u' ')
-                if idx > rindex:
-                    rindex = idx
-                if rindex > 0:
-                    if slice[rindex] == u' ':
-                        adjust = 1
-                    slice = slice[0:rindex]
-
-            self.lines.append(slice)
-            text_left = text_left[len(slice)+adjust:]
+        paragraphs = self.text.split("\n")
+        width = self.attrs['width'] - self.attrs['scrollbar_width'] - self.attrs['margin_left'] - self.attrs['margin_right']
+        for text_left in paragraphs:
+            while len(text_left) > 0:
+                bounding, to_right, fits = self.canvas.measure_text(text_left,
+                                                             font=(self.attrs['font'],self.attrs['font_size'],FONT_ANTIALIAS),
+                                                             maxwidth=width,
+                                                             maxadvance=width)
+                if fits <= 0:
+                    self.lines.append(text_left)
+                    break
+                    
+                slice = text_left[0:fits]
+                adjust = 0 # (preserve or not whitespaces at the end of the row)
+                if len(slice) < len(text_left):
+                    # find the separator character closest to the right
+                    rindex = -1
+                    idx = slice.rfind(u' ')
+                    if idx > rindex:
+                        rindex = idx
+                    if rindex > 0:
+                        if slice[rindex] == u' ':
+                            adjust = 1
+                        slice = slice[0:rindex]
+                
+                self.lines.append(slice)
+                text_left = text_left[len(slice)+adjust:]
 
     def set_text(self, text):
         self.text = text
@@ -139,3 +164,5 @@ class PWTextViewer(PWidget):
     def update_canvas(self):
         self.draw_background()
         self.draw_text()
+        self.draw_cursor()
+        self.draw_scrollbar()
